db5cb6425e7057b9b5c148ca8fed5c6ddf36db7a
elite_hzy <892621684@qq.com>
2023-05-30 23:29:32 +0800

    day1

---
+++ src/main/java/abc/controller/Abc.java
@@ -0,0 +1,10 @@
+package abc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/abc")
+public class Abc {
+
+}
